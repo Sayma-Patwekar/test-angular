8d6b67c65889f3124caee07392befc3a1bb13c52
Sayma-Patwekar <77375869+Sayma-Patwekar@users.noreply.github.com>
2023-03-03 11:25:52 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,8 +4,8 @@ sonarqubeScanner({
   serverUrl: 'http://localhost:9000 ',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.projectName': 'test1',
+	    'sonar.projectKey':'test1',
 	    //'sonar.login': 'squ_c33d0dae411ad2a52d4a556b64714c5cef790fb3',
 	    'sonar.login': 'admin',
 	    'sonar.password': 'sonarqube',
